d4bafe7fb1053e2e69bef389175c7dc6aa194071
Michael Klein <michaelklein@Michaels-MacBook-Pro.local>
2020-07-11 14:46:48 -0400

    Reworked button functionality to allow decimals

--- Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,87 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var result: EditText
+    private lateinit var newNumber: EditText
+    private val displayOperation by lazy(LazyThreadSafetyMode.NONE) { findViewById<TextView>(R.id.operation)}
+
+    //Variables to hold operands and type of calculation
+    private var operand1: Double? = null
+    private var operand2: Double = 0.0
+    private var pendingOperation = "="
+    private var dec: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        result = findViewById(R.id.result)
+        newNumber = findViewById(R.id.newNumber)
+
+        //Data input numbers
+        val button0: Button = findViewById(R.id.button0)
+        val button1: Button = findViewById(R.id.button1)
+        val button2: Button = findViewById(R.id.button2)
+        val button3: Button = findViewById(R.id.button3)
+        val button4: Button = findViewById(R.id.button4)
+        val button5: Button = findViewById(R.id.button5)
+        val button6: Button = findViewById(R.id.button6)
+        val button7: Button = findViewById(R.id.button7)
+        val button8: Button = findViewById(R.id.button8)
+        val button9: Button = findViewById(R.id.button9)
+        val buttonDec: Button = findViewById(R.id.buttonDec)
+        val buttonClear: Button = findViewById(R.id.buttonClear)
+
+        //Operation buttons
+        val buttonEquals: Button = findViewById(R.id.buttonEquals)
+        val buttonPlus: Button = findViewById(R.id.buttonPlus)
+        val buttonMinus: Button = findViewById(R.id.buttonMinus)
+        val buttonDivide: Button = findViewById(R.id.buttonDivide)
+        val buttonMultiply: Button = findViewById(R.id.buttonMultiply)
+
+        //Listener
+        val listener = View.OnClickListener { v ->
+
+            //TODO: Clean up button creation/make function to make code more resuable
+            val b = v as Button
+            if(b.text == "C"){
+                newNumber.text.clear()
+                if(dec == true){
+                    dec = false
+                }
+            }
+            else if(b.text == ".") {
+                if (dec == false) {
+                    dec = true
+                    newNumber.append(b.text)
+                }
+                else{
+                    return@OnClickListener
+                }
+            }
+            else {
+                newNumber.append(b.text)
+            }
+
+        }
+
+        button0.setOnClickListener(listener)
+        button1.setOnClickListener(listener)
+        button2.setOnClickListener(listener)
+        button3.setOnClickListener(listener)
+        button4.setOnClickListener(listener)
+        button5.setOnClickListener(listener)
+        button6.setOnClickListener(listener)
+        button7.setOnClickListener(listener)
+        button8.setOnClickListener(listener)
+        button9.setOnClickListener(listener)
+        buttonDec.setOnClickListener(listener)
+        buttonClear.setOnClickListener(listener)
     }
 }
